1112ca4690321c100d41f5fd8c2d5674439c6715
Gaurav S <gaurav@imbio.com>
2025-06-24 22:35:49 +0530

    added files

---
+++ task_1.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+
+print(f"Addition: {add}\nSubtraction: {sub}\nMultiplication: {mul}\nDivision: {div}")
